14eb8fa3c88c99e0b76338c3cb48bf4c94f205f3
Caner Candan <caner@candan.fr>
2013-10-24 23:26:11 +0200

    * migrations

---
+++ common/migrations/0002_auto__add_criteriatranslation__add_unique_criteriatranslation_language.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'CriteriaTranslation'
+        db.create_table('common_criteria_translation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('language_code', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=15)),
+            ('master', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['common.Criteria'], null=True, related_name='translations')),
+        ))
+        db.send_create_signal('common', ['CriteriaTranslation'])
+
+        # Adding unique constraint on 'CriteriaTranslation', fields ['language_code', 'master']
+        db.create_unique('common_criteria_translation', ['language_code', 'master_id'])
+
+        # Deleting field 'Criteria.name'
+        db.delete_column('common_criteria', 'name')
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'CriteriaTranslation', fields ['language_code', 'master']
+        db.delete_unique('common_criteria_translation', ['language_code', 'master_id'])
+
+        # Deleting model 'CriteriaTranslation'
+        db.delete_table('common_criteria_translation')
+
+        # Adding field 'Criteria.name'
+        db.add_column('common_criteria', 'name',
+                      self.gf('django.db.models.fields.CharField')(unique=True, default='', max_length=100),
+                      keep_default=False)
+
+
+    models = {
+        'common.address': {
+            'Meta': {'object_name': 'Address'},
+            'city': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'related_name': "'+'"}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Country']", 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'gender': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '1'}),
+            'home_phone': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'mobile_phone': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'postal_code': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
+            'street': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'})
+        },
+        'common.country': {
+            'Meta': {'object_name': 'Country'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'common.criteria': {
+            'Meta': {'object_name': 'Criteria'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'common.criteriatranslation': {
+            'Meta': {'db_table': "'common_criteria_translation'", 'unique_together': "[('language_code', 'master')]", 'object_name': 'CriteriaTranslation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language_code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '15'}),
+            'master': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['common.Criteria']", 'null': 'True', 'related_name': "'translations'"}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'common.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'exchange_rate': ('django.db.models.fields.FloatField', [], {'default': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'symbol': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'common.image': {
+            'Meta': {'object_name': 'Image'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'related_name': "'+'"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '200'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'common.parameter': {
+            'Meta': {'unique_together': "(('site', 'name'),)", 'object_name': 'Parameter'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'db_table': "'django_content_type'", 'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType'},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'sites.site': {
+            'Meta': {'db_table': "'django_site'", 'ordering': "('domain',)", 'object_name': 'Site'},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['common']
