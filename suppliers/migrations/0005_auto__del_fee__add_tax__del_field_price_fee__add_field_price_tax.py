61c12695725c8960b11c8d2899de0e7e827cb79f
Caner Candan <caner@candan.fr>
2013-10-24 19:21:20 +0200

    + migrations

---
+++ suppliers/migrations/0005_auto__del_fee__add_tax__del_field_price_fee__add_field_price_tax.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Fee'
+        db.delete_table('suppliers_fee')
+
+        # Adding model 'Tax'
+        db.create_table('suppliers_tax', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(null=True, blank=True, max_length=100)),
+            ('rate', self.gf('django.db.models.fields.FloatField')()),
+        ))
+        db.send_create_signal('suppliers', ['Tax'])
+
+        # Deleting field 'Price.fee'
+        db.delete_column('suppliers_price', 'fee_id')
+
+        # Adding field 'Price.tax'
+        db.add_column('suppliers_price', 'tax',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='supplier_product_price_tax', null=True, to=orm['suppliers.Tax'], blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Adding model 'Fee'
+        db.create_table('suppliers_fee', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('percent', self.gf('django.db.models.fields.FloatField')()),
+            ('name', self.gf('django.db.models.fields.CharField')(null=True, blank=True, max_length=100)),
+        ))
+        db.send_create_signal('suppliers', ['Fee'])
+
+        # Deleting model 'Tax'
+        db.delete_table('suppliers_tax')
+
+        # Adding field 'Price.fee'
+        db.add_column('suppliers_price', 'fee',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='supplier_product_price_fee', null=True, to=orm['suppliers.Fee'], blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Price.tax'
+        db.delete_column('suppliers_price', 'tax_id')
+
+
+    models = {
+        'accounts.account': {
+            'Meta': {'object_name': 'Account'},
+            'email': ('django.db.models.fields.EmailField', [], {'db_index': 'True', 'max_length': '255', 'unique': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'accounts.author': {
+            'Meta': {'object_name': 'Author'},
+            'account': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['accounts.Account']", 'unique': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_image': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['common.Image']", 'unique': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'})
+        },
+        'common.criteria': {
+            'Meta': {'object_name': 'Criteria'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
+        },
+        'common.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'exchange_rate': ('django.db.models.fields.FloatField', [], {'default': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'}),
+            'symbol': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'common.image': {
+            'Meta': {'object_name': 'Image'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '200'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'db_table': "'django_content_type'", 'object_name': 'ContentType', 'unique_together': "(('app_label', 'model'),)"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'products.category': {
+            'Meta': {'object_name': 'Category'},
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'null': 'True', 'to': "orm['accounts.Author']", 'symmetrical': 'False'}),
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'categories_rel_+'", 'null': 'True', 'to': "orm['products.Category']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_image': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['common.Image']", 'unique': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True'})
+        },
+        'products.product': {
+            'Meta': {'object_name': 'Product'},
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['products.Category']", 'symmetrical': 'False', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_image': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['common.Image']", 'unique': 'True', 'blank': 'True'}),
+            'products_parent': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'products_children'", 'null': 'True', 'to': "orm['products.Product']", 'symmetrical': 'False', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1', 'default': "'d'"})
+        },
+        'products.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'tag': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        'suppliers.entry': {
+            'Meta': {'object_name': 'Entry'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Order']"}),
+            'product': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['suppliers.Product']", 'unique': 'True'}),
+            'quantity': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'suppliers.inventory': {
+            'Meta': {'object_name': 'Inventory'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'product': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['suppliers.Product']", 'unique': 'True'}),
+            'quantity': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'store': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Store']"})
+        },
+        'suppliers.order': {
+            'Meta': {'object_name': 'Order'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1', 'default': "'d'"})
+        },
+        'suppliers.price': {
+            'Meta': {'object_name': 'Price', 'unique_together': "(('product', 'supplier', 'currency'),)"},
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'supplier_product_price_currency'", 'to': "orm['common.Currency']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Product']"}),
+            'purchase_price': ('django.db.models.fields.FloatField', [], {}),
+            'reference': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '30'}),
+            'selling_price': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'supplier': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Supplier']"}),
+            'supplier_product_url': ('django.db.models.fields.URLField', [], {'null': 'True', 'blank': 'True', 'max_length': '200'}),
+            'tax': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'supplier_product_price_tax'", 'null': 'True', 'to': "orm['suppliers.Tax']", 'blank': 'True'})
+        },
+        'suppliers.product': {
+            'Meta': {'object_name': 'Product'},
+            'criterias': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'product_criterias'", 'null': 'True', 'to': "orm['common.Criteria']", 'symmetrical': 'False', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_image': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['common.Image']", 'unique': 'True', 'blank': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'product_product'", 'to': "orm['products.Product']"}),
+            'sku': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100', 'unique': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1', 'default': "'d'"}),
+            'suppliers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'product_suppliers'", 'null': 'True', 'to': "orm['suppliers.Supplier']", 'symmetrical': 'False', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'suppliers.producttranslation': {
+            'Meta': {'db_table': "'suppliers_product_translation'", 'object_name': 'ProductTranslation', 'unique_together': "[('language_code', 'master')]"},
+            'body': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ingredients': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'language_code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '15'}),
+            'master': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'translations'", 'null': 'True', 'to': "orm['suppliers.Product']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
+        },
+        'suppliers.store': {
+            'Meta': {'object_name': 'Store'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_last_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'})
+        },
+        'suppliers.supplier': {
+            'Meta': {'object_name': 'Supplier'},
+            'delivery_delay': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_image': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'+'", 'null': 'True', 'to': "orm['common.Image']", 'unique': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'unique': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True'}),
+            'suppliers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'suppliers_rel_+'", 'null': 'True', 'to': "orm['suppliers.Supplier']", 'blank': 'True'}),
+            'threshold_order': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'suppliers.supplierfee': {
+            'Meta': {'object_name': 'SupplierFee', 'unique_together': "(('supplier', 'currency'),)"},
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'supplier_fee_currency'", 'to': "orm['common.Currency']"}),
+            'fee_per_weight': ('django.db.models.fields.FloatField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'supplier': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['suppliers.Supplier']"})
+        },
+        'suppliers.tax': {
+            'Meta': {'object_name': 'Tax'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'null': 'True', 'blank': 'True', 'max_length': '100'}),
+            'rate': ('django.db.models.fields.FloatField', [], {})
+        }
+    }
+
+    complete_apps = ['suppliers']
