a3a6046ee5c471221560e7df190a8e4fab6c88ef
Caner Candan <caner@candan.fr>
2013-11-22 16:54:35 +0100

    * accounts/management/commands/get_email_list.py

--- accounts/management/commands/get_email_list.py
+++ accounts/management/commands/get_email_list.py
@@ -1,10 +1,28 @@
-from ... import models
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# Authors:
+# Caner Candan <caner@candan.fr>, http://caner.candan.fr
+# Geraldine Starke <geraldine@starke.fr>, http://www.vegeclic.fr
+#
+
 from django.core.management.base import BaseCommand, CommandError, NoArgsCommand
+from ... import models
 
 class Command(NoArgsCommand):
     help = 'Get a list of the accounts email'
 
     def handle_noargs(self, **options):
         for a in models.Account.objects.all():
-                print(a.email, end=' ')
-
+            print(a.email, end=' ')
