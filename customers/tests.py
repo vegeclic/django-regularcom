d28a0f54a8e6988f40eb7f00b722172eed2b8fbc
Caner Candan <caner@candan.fr>
2014-01-08 23:35:24 +0100

    * customers/tests.py: added new tests

--- customers/tests.py
+++ customers/tests.py
@@ -19,44 +19,52 @@
 
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
+from django.core import mail
 from . import models
 import accounts.models as am
 import common.models as cm
 
-def create_customer(email):
-    account, account_created = am.Account.objects.get_or_create(email=email)
-    customer, customer_created = models.Customer.objects.get_or_create(account=account)
-    return customer
+EMAIL_ADMIN, EMAIL, PASSWORD = 'admin@admin.xyz', 'john@smith.xyz', 'mypassword'
+
+def create_customer(email, password=''):
+    account = am.Account.objects.create_user(email)
+    if password:
+        account.set_password(password)
+        account.save()
+    return account.customer
 
 def create_address(customer, first_name, last_name):
     return cm.Address.objects.create(content_type=ContentType.objects.get(model='customer'), object_id=customer.id, first_name=first_name, last_name=last_name)
 
-def create_customer_with_main_address(email, first_name, last_name):
-    customer = create_customer(email)
+def create_customer_with_main_address(email, first_name, last_name, password=''):
+    customer = create_customer(email, password)
     customer.main_address = create_address(customer, first_name, last_name)
     customer.save()
     return customer
 
+@override_settings(EMAIL_ADMIN=EMAIL_ADMIN)
 class CustomerMethodTests(TestCase):
     def test_get_shipping_address_when_empty(self):
         """
         get_shipping_address() should return main_address for customers whose shipping_address is empty.
         """
-        customer = create_customer_with_main_address('john@smith.xyz', 'John', 'Smith')
+        customer = create_customer_with_main_address(EMAIL, 'John', 'Smith')
         self.assertEqual(customer.get_shipping_address(), customer.main_address)
 
     def test_get_billing_address_when_empty(self):
         """
         get_billing_address() should return main_address for customers whose billing_address is empty.
         """
-        customer = create_customer_with_main_address('john@smith.xyz', 'John', 'Smith')
+        customer = create_customer_with_main_address(EMAIL, 'John', 'Smith')
         self.assertEqual(customer.get_billing_address(), customer.main_address)
 
     def test_get_shipping_address_when_not_empty(self):
         """
         get_shipping_address() should return shipping_address for customers whose shipping_address is not empty.
         """
-        customer = create_customer('john@smith.xyz')
+        customer = create_customer(EMAIL)
         customer.shipping_address = create_address(customer, 'Jojo', 'Smismi')
         customer.save()
         self.assertEqual(customer.get_shipping_address(), customer.shipping_address)
@@ -65,7 +73,7 @@ class CustomerMethodTests(TestCase):
         """
         get_billing_address() should return billing_address for customers whose billing_address is not empty.
         """
-        customer = create_customer('john@smith.xyz')
+        customer = create_customer(EMAIL)
         customer.billing_address = create_address(customer, 'Jojo', 'Smismi')
         customer.save()
         self.assertEqual(customer.get_billing_address(), customer.billing_address)
@@ -74,14 +82,39 @@ class CustomerMethodTests(TestCase):
         """
         is_pro() should return False for customers created with default behavior.
         """
-        customer = create_customer('john@smith.xyz')
+        customer = create_customer(EMAIL)
         self.assertEqual(customer.is_pro(), False)
 
     def test_is_pro_when_customer_is_pro(self):
         """
         is_pro() should return True for customers created with the pro status.
         """
-        customer = create_customer('john@smith.xyz')
+        customer = create_customer(EMAIL)
         customer.pro = True
         customer.save()
         self.assertEqual(customer.is_pro(), True)
+
+@override_settings(EMAIL_ADMIN=EMAIL_ADMIN)
+class CustomerDetailViewTests(TestCase):
+    def test_detail_view_isequal_to_customer_object(self):
+        """
+        Check that context['object'] is equal to customer object.
+        """
+        customer = create_customer(EMAIL, PASSWORD)
+        self.assertEqual(len(mail.outbox), 1)
+        self.client.get(reverse('login_link', args=(EMAIL, PASSWORD)))
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.context['object'], customer)
+
+    def test_detail_view_without_addresses(self):
+        """
+        Ensure that the customer addresses are unset.
+        """
+        customer = create_customer(EMAIL, PASSWORD)
+        self.assertEqual(len(mail.outbox), 1)
+        self.client.get(reverse('login_link', args=(EMAIL, PASSWORD)))
+        response = self.client.get(reverse('profile'))
+        obj = response.context['object']
+        self.assertIsNone(obj.main_address)
+        self.assertIsNone(obj.shipping_address)
+        self.assertIsNone(obj.billing_address)
