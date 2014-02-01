8e51f0a9a9adad7b61afbb30fbbbe7e5b3c4de33
Caner Candan <caner@candan.fr>
2014-02-01 20:01:01 +0100

    * accounts/tests.py: added test file

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,16 +1,39 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# Authors:
+# Caner Candan <caner@candan.fr>, http://caner.candan.fr
+# Geraldine Starke <geraldine@starke.fr>, http://www.vegeclic.fr
+#
 
-Replace this with more appropriate tests for your application.
-"""
+from django.test import TestCase, LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from django.core.urlresolvers import reverse
 
-from django.test import TestCase
+class AccountMethodTest(TestCase):
+    pass
 
+# class AccountSeleniumTests(LiveServerTestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.selenium = WebDriver()
+#         super().setUpClass()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.selenium.quit()
+#         super().tearDownClass()
+
+#     def test_login(self):
+#         self.selenium.get('%s%s' % (self.live_server_url, reverse('login')))
